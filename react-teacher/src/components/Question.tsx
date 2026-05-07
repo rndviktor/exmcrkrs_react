0f98e508ef9fb2e75978888fbc907469a4877ad1
rndviktor <rnd4job@gmail.com>
2026-05-07 23:09:01 +0200

    feat: intro of minimap for questions content

--- react-teacher/src/components/Question.tsx
+++ react-teacher/src/components/Question.tsx
@@ -11,9 +11,11 @@ const Question = ({ examId, question }: { examId: string | null, question: Quest
     return <>
         <div className="text-[10px] font-light text-gray-400 pl-1 leading-none mb-1">{question.QuestionId}</div>
         <div className="w-full flex flex-1 items-center gap-4 mb-2">
-            <div className="whitespace-pre-line border-b-2 border-l-2 border-t-4 border-t-cyan-600 flex-1 question-content p-2"
-                 dangerouslySetInnerHTML={{ __html: question.Content! }}
-            />
+            <div className={classes.minimapContainer}>
+                <div className={`whitespace-pre-line question-content ${classes.minimapContent}`}
+                     dangerouslySetInnerHTML={{ __html: question.Content! }}
+                />
+            </div>
             <Link id={qId} className={classes.button} to={`/exam/${examId}/editQuestion/${question.QuestionId}`}>
                 <MdEdit size={16} />
             </Link>
